c9205f822ea4e5fcf1e0237e03f0f7ad0242c7ff
Nayaabkhan Khan <khannayaab@gmail.com>
2019-03-26 11:26:25 +0530

    Updates

---
+++ precache-manifest.db2798e74601b16a260ddf91792931f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nayaabkhan.github.io/github-hosted-react-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e3ec677d4175b1b262a",
+    "url": "/nayaabkhan.github.io/github-hosted-react-site/static/js/runtime~main.5a4f8031.js"
+  },
+  {
+    "revision": "f4dabb0c211a7d64cb74",
+    "url": "/nayaabkhan.github.io/github-hosted-react-site/static/js/main.4deb4022.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/nayaabkhan.github.io/github-hosted-react-site/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "f4dabb0c211a7d64cb74",
+    "url": "/nayaabkhan.github.io/github-hosted-react-site/static/css/main.bceedb3c.chunk.css"
+  },
+  {
+    "revision": "d4c98a1526e6242cf6501a14b5e34189",
+    "url": "/nayaabkhan.github.io/github-hosted-react-site/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nayaabkhan.github.io/github-hosted-react-site/precache-manifest.db2798e74601b16a260ddf91792931f4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nayaabkhan.github.io/github-hosted-react-site/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
